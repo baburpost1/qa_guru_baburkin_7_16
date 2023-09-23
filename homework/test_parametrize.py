bf25d18082f568f4415b7420369d9324b0435161
Artem <a.baburkin@it.adcombo.com>
2023-09-23 18:04:28 +0300

    test-parametrize

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,16 +2,28 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+import selene
+from selene import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+# selene.browser.config.timeout = 4
 
 
+@pytest.fixture(params=[{"height": 3860, "width": 2180}])
+def browser(request):
+    browser = selene.browser
+    browser.config.window_height = request.param['height']
+    browser.config.window_width = request.param['width']
+    return browser
+
+
+@pytest.mark.parametrize('browser', [{"height": 1000, "width": 2000}, {"height": 1000, "width": 1600}], indirect=True)
 def test_github_desktop(browser):
-    pass
+    main_page = browser.open("https://github.com")
+    main_page.element('[href="/login"]').click()
 
-
+@pytest.mark.parametrize('browser', [{"height": 1000, "width": 1000},{"height": 896, "width": 414}], indirect=True)
 def test_github_mobile(browser):
-    pass
+    main_page = browser.open("https://github.com")
+    main_page.element('[class="flex-1 flex-order-2 text-right"] [aria-label="Toggle navigation"]').click()
+    main_page.element('[href="/login"]').click()
