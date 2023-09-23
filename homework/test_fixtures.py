df2b2dc3d68ee92743fb388113b36f7cf84d57c1
Artem <a.baburkin@it.adcombo.com>
2023-09-23 16:05:31 +0300

    test-fixtures

--- homework/test_fixtures.py
+++ homework/test_fixtures.py
@@ -1,22 +1,23 @@
 import pytest
-from selene import browser
+from selene import browser, Browser
 
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
 
 
-@pytest.fixture(params=['height', 'width'])
-def set_desktop_size(request):
-    browser.config.window_height = pytest.param('height')
-    browser.config.window_width = pytest.param('width')
+@pytest.fixture()
+def set_desktop_size():
+    browser.config.window_height = 2160
+    browser.config.window_width = 3840
+
 
 @pytest.fixture()
 def set_mobile_size():
     browser.config.window_height = 896
     browser.config.window_width = 414
 
-@pytest.mark.parametrize("set_desktop_size", [(2160, 3840), (1111, 356)], indirect=True)
+
 def test_github_desktop(set_desktop_size):
     main_page = browser.open("https://github.com")
     main_page.element('[href="/login"]').click()
